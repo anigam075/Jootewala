1e2fbd0a0b0212174ef5ddbb73ee0e3dfc0ca0bb
arpit <anigam075@gmail.com>
2024-05-14 17:54:21 +0530

    new changes

--- jootewalapp/views.py
+++ jootewalapp/views.py
@@ -82,3 +82,73 @@ def user_register_f(request):
             'Remark': 'Landed in Exception',
         }
         raise APIException(json_data)
+    
+
+
+@api_view(['POST'])
+@authentication_classes((TokenAuthentication,))
+@permission_classes((IsAuthenticated,))
+def user_login_f(request):
+    try:
+        serializer = UserProfileInsertSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.data['Email']
+            email_exists = check_existing_email_q(email)
+            if not email_exists:
+                Data = {
+                    'UserID': str(uuid.uuid4()),
+                    'FirstName': serializer.data['FirstName'],
+                    'LastName': serializer.data['LastName'],
+                    'Email': serializer.data['Email'],
+                    'Password': serializer.data['Password'],
+                    'Phone': serializer.data['Phone'],
+                    'Address': serializer.data['Address'],
+                    'City': serializer.data['City'],
+                    'Country': serializer.data['Country'],
+                    'Pincode': serializer.data['Pincode'],
+                    'Isdeleted': 0,
+                    'CreatedAt': datetime.datetime.now()
+                }
+                insert = user_insert_q(list(Data.values()))
+                if insert:
+                    json_data = {
+                        'status_code': 200,
+                        'status': 'Success',
+                        'data': '',
+                        'message': 'Data Inserted Successfully',
+                    }
+                    return Response(json_data, status=stus.HTTP_200_OK)
+                else:
+                    json_data = {
+                        'status_code': 200,
+                        'status': 'Success',
+                        'data': '',
+                        'message': 'Data Inserted Successfully',
+                    }
+                    return Response(json_data, status=stus.HTTP_200_OK)
+            else:
+                json_data = {
+                    'status_code': 200,
+                    'status': 'Failed',
+                    'Reason': "Duplicate Email Entered",
+                    'Remark': 'Email Already Exists',
+                }
+                return Response(json_data, status=stus.HTTP_200_OK)
+        else:
+            json_data = {
+                'status_code': 300,
+                'status': 'Failed',
+                'data': serializer.errors,
+                'message': 'Please Enter All Fields'
+            }
+            return Response(json_data, status=stus.HTTP_300_MULTIPLE_CHOICES)
+    except Exception as e:
+        json_data = {
+            'status_code': 400,
+            'status': 'Failed',
+            'Reason': e,
+            'Remark': 'Landed in Exception',
+        }
+        raise APIException(json_data)
+    
+
